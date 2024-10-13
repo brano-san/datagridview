8fc8c49e999d440850d9858e00a671c36ff7286b
brano-san <branoverv10@bk.ru>
2024-10-13 21:03:20 +0300

    Add data validations

---
+++ datagridview/Forms/EditTourForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using datagridview.src;
+
+namespace datagridview.Forms
+{
+    public partial class EditTourForm : Form
+    {
+        public Contracts.Models.Tour CurrentTour { get; private set; }
+
+        public EditTourForm(Contracts.Models.Tour tour = null)
+        {
+            InitializeComponent();
+
+            if (tour != null)
+            {
+                CurrentTour = new Contracts.Models.Tour
+                {
+                    Id = tour.Id,
+                    AdditionalFees = tour.AdditionalFees,
+                    DepartureDate = tour.DepartureDate,
+                    Destination = tour.Destination,
+                    HasWifi = tour.HasWifi,
+                    Nights = tour.Nights,
+                    PeopleCount = tour.PeopleCount,
+                    PricePerTour = tour.PricePerTour,
+                };
+            }
+            else
+            {
+                CurrentTour = new Contracts.Models.Tour
+                {
+                    Id = Guid.NewGuid(),
+                    DepartureDate = DateTime.Now,
+                };
+            }
+
+            AddBindings();
+            UpdateData();
+        }
+
+        private void AddBindings()
+        {
+            txtDestination.AddBinding(x => x.Text, CurrentTour, x => x.Destination, errorProvider1);
+            dtpDepartureDate.AddBinding(x => x.Value, CurrentTour, x => x.DepartureDate, errorProvider1);
+            nudNights.AddBinding(x => x.Value, CurrentTour, x => x.Nights, errorProvider1);
+            nudPricePerTour.AddBinding(x => x.Value, CurrentTour, x => x.PricePerTour, errorProvider1);
+            nudPeopleCount.AddBinding(x => x.Text, CurrentTour, x => x.PeopleCount, errorProvider1);
+            chkWifi.AddBinding(x => x.Checked, CurrentTour, x => x.HasWifi, errorProvider1);
+            nudAdditionalFees.AddBinding(x => x.Value, CurrentTour, x => x.AdditionalFees, errorProvider1);
+        }
+
+        private void UpdateData()
+        {
+            txtCount.Text = (nudPeopleCount.Value * nudPricePerTour.Value + nudAdditionalFees.Value).ToString();
+        }
+
+        private void UpdateData(object sender, EventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (errorProvider1.ContainerControl.Controls.Cast<Control>().Any(c => errorProvider1.GetError(c) != ""))
+            {
+                MessageBox.Show("Некорректные данные", "Ошибка");
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
---
+++ datagridview/Forms/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Forms;
+using datagridview.Contracts;
+using datagridview.Forms;
+
+namespace datagridview
+{
+    public partial class MainForm : Form
+    {
+        private ITourManager tourManager;
+        private BindingSource bindingSource;
+
+        public MainForm(ITourManager tourManager)
+        {
+            InitializeComponent();
+
+            this.tourManager = tourManager;
+
+            bindingSource = new BindingSource();
+            dataGridView1.DataSource = bindingSource;
+
+            UpdateToolStrip();
+        }
+
+        private async void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count <= 0)
+            {
+                return;
+            }
+            var selectedTour = (Contracts.Models.Tour)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].DataBoundItem;
+            var form = new EditTourForm(selectedTour);
+            form.ShowDialog();
+
+            await tourManager.EditTourAsync(form.CurrentTour);
+            bindingSource.ResetBindings(false);
+            UpdateToolStrip();
+        }
+
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            var form = new EditTourForm();
+            if (form.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            await tourManager.AddTourAsync(form.CurrentTour);
+            bindingSource.ResetBindings(false);
+            UpdateToolStrip();
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                return;
+            }
+
+            var selectedTour = (Contracts.Models.Tour)dataGridView1.CurrentRow.DataBoundItem;
+
+            await tourManager.DeleteTourAsync(selectedTour.Id);
+            bindingSource.ResetBindings(false);
+            UpdateToolStrip();
+        }
+
+        private void UpdateToolStrip()
+        {
+            var toursCount = dataGridView1.RowCount;
+            var sum = 0.0;
+            var toursCountWithAdd = 0;
+            var additionSum = 0.0;
+
+            for (var i = 0; i < dataGridView1.RowCount; i++)
+            {
+                var tour = (Contracts.Models.Tour)dataGridView1.Rows[i].DataBoundItem;
+                sum += tour.TotalCost;
+
+                if (tour.AdditionalFees != 0)
+                {
+                    ++toursCountWithAdd;
+                    additionSum += tour.AdditionalFees;
+                }
+            }
+
+            strip1.Text = $"Количество туров {toursCount};";
+            strip2.Text = $"Общая сумма {sum}; ";
+            strip3.Text = $"Количество туров с доплатами {toursCountWithAdd}; ";
+            strip4.Text = $"Общая сумма доплат {additionSum}; ";
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            bindingSource.DataSource = await tourManager.GetAllToursAsync();
+            dataGridView1.Columns[nameof(Contracts.Models.Tour.Id)].Visible = false;
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "TotalCost")
+            {
+                var data = (Contracts.Models.Tour)dataGridView1.Rows[e.RowIndex].DataBoundItem;
+                data.TotalCost = data.PricePerTour * data.PeopleCount + data.AdditionalFees;
+                e.Value = data.TotalCost;
+            }
+            UpdateToolStrip();
+        }
+    }
+}
